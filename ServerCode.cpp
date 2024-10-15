71039388ca8b368e5f8273f9eab03d66750ece6c
pham022 <124918196+pham022@users.noreply.github.com>
2024-10-15 06:26:44 -0500

    Update ServerCode.cpp
    Added a timeout timer with a reminder at the halfway point to notify the user

--- ServerCode.cpp
+++ ServerCode.cpp
@@ -5,6 +5,10 @@ const char* password = "12345678";
 
 WiFiServer server(80);
 
+unsigned long timeout = 30000;
+unsigned long lastActivityTime = 0;
+bool reminderSent = false;
+
 void setup() {
   Serial.begin(115200);
 
@@ -26,18 +30,39 @@ void loop() {
   if (client) {
     Serial.println("New client connected");
 
+    // Reset the timer when the client connects
+    lastActivityTime = millis();
+
     while (client.connected()) {
       if (client.available()) {
         String message = client.readStringUntil('\n');
+        message.trim();
         Serial.println("Received message: " + message);
 
-        // Send response to client
-        server.println("Message received");
+        // Send response to client so it has to be client.println()
+        client.println("Message is succesfully received in the server side");
+
+         // Reset the activity timer since we've received new message
+        lastActivityTime = millis();
+        reminderSent = false;
       }
+      // send a reminder after half of the timeout passed
+      if ((millis() - lastActivityTime >= timeout / 2) && !reminderSent) {
+        Serial.println("I'm still listening...");
+        client.println("Server: I'm still listening...");  
+        reminderSent = true; 
+      }
+
+      if (millis() - lastActivityTime > timeout) {
+        Serial.println("Timeout reached, disconnecting client");
+        client.stop();  
+        break;
+      }
+      // I removed delay to allow the loop to check for data continuously
     }
 
-    // Close the connection
-    client.stop();
-    Serial.println("Client disconnected");
+    if (!client.connected()) {
+      Serial.println("Client disconnected");
+    }
   }
 }
